78a8158edf54acaec5474eaf14a5c665064d8ed0
sudhanshu <inderssingh4732@gmail.com>
2023-07-26 01:32:21 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+ // we can make  jason file by using npm it key word in terminal
+ // then we have to install express and pug
+
+const express =require("express");
+const fs=require("fs");
+const  app = express();
+ const path=require("path");
+  port=8000;
+
+  // EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+})
